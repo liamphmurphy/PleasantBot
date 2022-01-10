3119451057099a4ce2938afe68c06805ed7c1e04
Liam Murphy <liam@phmurphy.com>
2022-01-09 19:37:57 -0800

    remove old script

--- src/pleasantbot-gui/vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    devServer: {
-      host: 'localhost',
-      port: 8081,
-      public: 'localhost:8081',
-    },
-  };
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# launch the bot
-cd ../ && go build && ./pleasantbot &
-
-# launch React front-end
-cd src/gui && npm start
