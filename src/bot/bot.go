5dfce711067ec0cd2fc233ea31ca9b2547e5537d
Liam Murphy <liam@phmurphy.com>
2022-01-16 08:16:01 -0800

    begin generalizing config management

--- src/bot/bot.go
+++ src/bot/bot.go
@@ -47,23 +47,6 @@ type Bot struct {
 	TimerDBColumns   []string                 `json:"-"`
 }
 
-// writeConfig is run whenever the config.toml file doesn't exist, usually after a fresh download of the bot.
-func writeConfig(path string, configObject *viper.Viper) {
-	path = path + "/config.toml"
-	// prepare default values, will be used when viper writes the new config file
-	configObject.SetDefault("ChannelName", "<enter channel name to moderate here>")
-	configObject.SetDefault("ServerName", "irc.chat.twitch.tv:6697")
-	configObject.SetDefault("BotName", "<enter bot username here>")
-	configObject.SetDefault("BotOAuth", "<bot oauth>")
-	configObject.SetDefault("PurgeForLinks", true)
-	configObject.SetDefault("PurgeForLongMsg", true)
-	configObject.SetDefault("LongMsgAmount", 400)
-	configObject.SetDefault("EnableServer", true)
-	configObject.SetDefault("PostLinkPerm", uint(1)) // Minimum permission needed for non-purging links, in this case subscriber
-
-	configObject.WriteConfigAs(path)
-}
-
 // CreateBot creates an instance of a bot. The caller needs to pass in where the config file should exist, the name of the config file,
 // and pass whether this path should be made if it doesn't exist already.
 // dbName will be the name of the database file located in the directory declared in configPath.
